ab919e3c982cd3a277b31425af71bb2cea502cea
Changhyeon Park <ch7170p@naver.com>
2021-12-16 14:11:38 +0900

    ROS2 Custom Message based Publisher

--- ros2_communication_ws/src/practice_code_pkg/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neubility/ROS/ros2_communication_ws/src/pub_ex_pkg
-# Build directory: /home/neubility/ROS/ros2_communication_ws/src/pub_ex_pkg/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
